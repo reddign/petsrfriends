af0e161564b31f2018cf61afbddc9f03fb74791e
devon <barnesd1@etown.edu>
2024-08-08 11:09:54 -0400

    footer and header

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<style>
+.footer {
+   position:fixed;
+   left:0px;
+   bottom:0px;
+   height:15px;
+   width:100%;
+   color: white;
+   background:#e3ba78;
+}
+</style>
+
+
+<BR><BR><BR><BR>
+<div class = "footer">
+Copyright Pets R Friends 2024
+</div>
+</body>
+</html>
